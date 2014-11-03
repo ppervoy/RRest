a1311a2de571cd79dfa6f8f5b8311efdb488848a
Pavel Pervoy <p.pervoy@gmail.com>
2014-11-03 18:24:26 +0200

    09 03. Guide class

--- init.rb
+++ init.rb
@@ -3,3 +3,6 @@ APP_ROOT = File.dirname(__FILE__)
 #require "#{APP_ROOT}/lib/guide"
 $:.unshift( File.join(APP_ROOT, 'lib') )
 require 'guide'
+
+g = Guide.new("Kiev.txt")
+g.launch!
--- lib/guide.rb
+++ lib/guide.rb
@@ -1,1 +1,25 @@
-
+class Guide
+	def initialize (path = nil)
+		# load
+		# create
+		# exit if failed
+	end
+	
+	def launch!
+		intro
+		# action loop
+		#  prompt
+		#  do
+		# repeat until qut
+		conclusion
+	end
+	
+	def intro
+		puts "Welcome to Ruby Restaurants (RRest)"
+		puts "the place to find things you love"
+	end
+	
+	def conclusion
+		puts "See you soon again!"
+	end
+end
